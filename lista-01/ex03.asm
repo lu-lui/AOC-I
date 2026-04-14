4f20cc3cb969587f109b04d111677f80116ebb52
lu-lui <lpneutzling@gmail.com>
2026-04-13 21:28:01 -0300

    exercicios

--- lista-01/ex03.asm
+++ lista-01/ex03.asm
@@ -6,13 +6,12 @@
 .text
 addi $t0, $zero, 3 #$t0 = 3
 addi $t1, $zero, -5 #$t1 = -5
-addi $t2, $zero, 13 #$t2 = 13
 addi $t5, $zero, 2 #$t5 = x = 2
 mult $t5, $t5 #$t5 * $t5 = 2 * 2 = 4
 mflo $t6 #t6 = 4
 mult $t0, $t6 # 3 * x² = 3 * 4 = 12
 mflo $t6 #$t6 = 12
-mult $t1, $t5 #5 * x = 5 * 2
+mult $t1, $t5 #-5 * x = -5 * 2
 mflo $t7 #t7 = -10
 add  $t6, $t7, $t6 #t6¨= 12 -10 = 2
-add  $t6, $t6, $t2 #$t6 = 2 + 13 = 15
+addi  $t6, $t6, 13 #$t6 = 2 + 13 = 15
