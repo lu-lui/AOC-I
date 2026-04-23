a92df30f60d151735b33ee1ba56e7dca02e09af5
lu-lui <lpneutzling@gmail.com>
2026-04-22 21:27:09 -0300

    exercícios
     instruções lógicas

--- lista-02/ex02.asm
+++ lista-02/ex02.asm
@@ -1,2 +1,27 @@
-#2. Escreva 0xAAAAAAAA em $t1. Faça um shift de um bit para a direita desse valor e coloque o resultado em $t2, deixando $t1 inalterado. Em $t3, $t4 e $t5 coloque os resultados das operações
-#or, and e xor entre $t1 e $t2, respectivamente. Em comentários no final do código, explique os resultados obtidos, mostrando os valores binários.
+#2. Escreva 0xAAAAAAAA em $t1. Faça um shift de um bit para a direita desse valor e coloque o resultado em $t2, deixando $t1 inalterado. 
+#Em $t3, $t4 e $t5 coloque os resultados das operações or, and e xor entre $t1 e $t2, respectivamente. 
+#Em comentários no final do código, explique os resultados obtidos, mostrando os valores binários.
+
+.text
+ori $t1, $zero, 0xAAAA #t1 = AAAA
+sll $t1, $t1, 16 #t1 = AAAA0000
+ori $t1, $t1, 0xAAAA #t1 = AAAAAAAA
+srl $t2, $t1, 1 #t1 = 55555555
+or  $t3, $t1, $t2 #t3 = ffffffff
+and $t4, $t1, $t2 #t1 = 00000000
+xor $t5, $t1, $t2 #t1 = ffffffff
+
+# or: se um dos bits comparados é 1, o resultado da or é 1. 
+# $t1 = AAAAAAAA = 10101010 10101010 10101010 10101010
+# $t2 = 55555555 = 01010101 01010101 01010101 01010101
+# $t3 = FFFFFFFF = 11111111 11111111 11111111 11111111
+
+# and: se os dois bits comparados forem 1, o resultado da and é 1. 
+# $t1 = AAAAAAAA = 10101010 10101010 10101010 10101010
+# $t2 = 55555555 = 01010101 01010101 01010101 01010101
+# $t3 = 00000000 = 00000000 00000000 00000000 00000000
+
+# xor: se a quantidade de 1s nos bits comparados for ímpar, o resultado da or é 1. 
+# $t1 = AAAAAAAA = 10101010 10101010 10101010 10101010
+# $t2 = 55555555 = 01010101 01010101 01010101 01010101
+# $t3 = FFFFFFFF = 11111111 11111111 11111111 11111111
--- lista-02/ex03.asm
+++ lista-02/ex03.asm
@@ -1,2 +1,17 @@
-#3. Comece um programa pela instrução: ori $t1, $zero, 0x01. Usando apenas as instruções lógicas do tipo R (dois registradores como operandos) or, and e xor e instruções de shift (isto é, sem
-#usar outras instruções ori além da inicial e nem instruções nor), escreva 0xFFFFFFFF em $t1. Procure usar o menor número de instruções possível.
+#3. Comece um programa pela instrução: ori $t1, $zero, 0x01. Usando apenas as instruções lógicas do tipo R (dois registradores como operandos) 
+#or, and e xor e instruções de shift (isto é, sem usar outras instruções ori além da inicial e nem instruções nor), escreva 0xFFFFFFFF em $t1. 
+#Procure usar o menor número de instruções possível.
+
+.text
+ori $t1, $zero, 0x01 #$t1 = 00000001
+sll $t2, $t1, 1 #t2 = 00000002
+or  $t1, $t1, $t2 #$t1 = 0000003
+sll $t2, $t1, 2 #t2 = 00001100
+or $t1, $t1, $t2 #t1 = 0000000c
+sll $t2, $t1, 4 #t2 = 0000000f
+sll $t2, $t1, 4 #t2 = 000000f0
+or $t1, $t1, $t2 #t1 = 000000ff
+sll $t2, $t1, 8 #t2 = 000000f0
+or $t1, $t1, $t2 #t1 = 0000ffff
+sll $t2, $t1, 16 #t2 = 000000f0
+or $t1, $t1, $t2 #t1 = ffffffff
