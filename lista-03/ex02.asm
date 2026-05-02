4a497a5d000f6fb1790455e9581fe21b6527c334
lu-lui <lpneutzling@gmail.com>
2026-05-02 16:51:40 -0300

    exercicios
    agr com horner de maneira correta

--- lista-03/ex02.asm
+++ lista-03/ex02.asm
@@ -11,23 +11,21 @@ y: .space 4
 lui $t0, 0x1001      #registrador-base $t0 = 0x10010000
 lw $t1,  0($t0)      #$t1 = 3
 
-addi $s0, $zero, 7  #$s0 = 7
-mult $s0, $t1       #7 * a = 7 * 3 = 21
-mflo $s0            #$s0 = 21
-addi $s0, $s0, 15   #$s0 = 36
+addi $s0, $zero, 9  #$s0 = 9
+mult $s0, $t1       #9 * 3 = 27
+mflo $s0            #$s0 = 27
 
-mult $t1, $t1 	     # a² = 3 * 3 = 9
-mflo $t2            #$t1 = 9
-addi $s1, $zero, -5 #$s1 = -5
-mult $s1, $t2       #5 * a² = 5 * 9 = -45
-mflo $s1            #$s1 = -45
-add  $s0, $s0, $s1  #$s0 = -9
+addi $s1, $zero, 5   #$s1 = 5
+sub $s0, $s0, $s1 #27 - 5 = 22
+mult $s0, $t1       #22 * 3 = 66
+mflo $s0            #$s0 = 66
 
-mult $t2, $t1       #a² * a = 27
-mflo $t2            #$t1 = 27
-addi $s1, $zero, 9  #$s1 = 9
-mult $s1, $t2       #9 * a³ = 9 * 27 = 243
-mflo $s1            #$s1 = 243
-add  $s0, $s0, $s1  #$s0 = 234
+addi $s1, $zero, 7   #$s1 = 7
+add $s0, $s0, $s1 #66 + 7 = 73
+mult $s0, $t1       #73 * 3 = 219
+mflo $s0            #$s0 = 219
+
+addi $s1, $zero, 15   #$s1 = 15
+add $s0, $s0, $s1 #219 + 15 = 234
 
 sw $s0 4($t0)       #0x10010004 = 234
--- lista-03/ex03.asm
+++ lista-03/ex03.asm
@@ -2,6 +2,7 @@
 #Utilize endereços de memória para armazenar o valor de a, b, c, d, e, x e o resultado y. 
 #Cada valor deve ocupar 4 bytes na memória (.word), assim como para o resultado (.space 4). 
 #Utilize as sete primeiras posições da memória .data para armazenar, consecutivamente, a, b, c, d, e, x e y, iniciando o código com:
+
 .data
 a: .word -3 #0x10010000
 b: .word 7 #0x10010004
@@ -10,3 +11,34 @@ d: .word -2 #0x1001000C
 e: .word 8 #0x10010010
 x: .word 4 #0x10010014
 y: .space 4 #0x10010018
+
+#y=((((−a)x+b)x−c)x+d)x−e
+
+.text
+lui $t0, 0x1001 #endereço-base
+lw $t1, 20($t0) #t1 = x = 4 
+
+lw $s0, 0($t0) #$s0 = a = -3
+sub $s0, $zero, $s0 #$s0 = -a = 3
+mult $s0, $t1 #3 * 4 = 12
+mflo $s0 #s0 = 12
+
+lw $s1, 4($t0) #$s1 = b = 7
+add $s0, $s0, $s1 #$s0 = 19
+mult $s0, $t1 #19 * 4 = 76
+mflo $s0 #s0 = 76
+
+lw $s1, 8($t0) #$s1 = c = 5
+sub $s0, $s0, $s1 #$s0 = 76 - 5 = 71
+mult $s0, $t1 #71 * 4 = 284
+mflo $s0 #s0 = 284
+
+lw $s1, 12($t0) #$s1 = d = -2
+add $s0, $s0, $s1 #$s0 = 284 + -2 = 282
+mult $s0, $t1 #282 * 4 = 1128
+mflo $s0 #s0 = 1128
+
+lw $s1, 16($t0) #$s1 = e = 8
+sub $s0, $s0, $s1 #$s0 = 1128 - 8 = 1120
+
+sw $s0, 24($t0)
