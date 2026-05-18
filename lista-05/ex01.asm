270a5ffbd7a482835d3c4138eaee3e8b652d1d8c
lu-lui <lpneutzling@gmail.com>
2026-05-18 14:11:50 -0300

    exercicios

--- lista-05/ex01.asm
+++ lista-05/ex01.asm
@@ -15,24 +15,28 @@ vetor: .space 32 #4 * 8 indices = 32
 .text
 lui $s0, 0x1001
 add $t0, $zero, $zero #$t0 = i = 0
-addi $t1, $zero, 8    #$t1 = contador = 8
+addi $t1, $zero, 8    #$t1 = limite = 8
 addi $t3, $zero, 2    #$t3 = 2
-addi $t4, $zero, -1   #$t4 = i-1 = -1
  
-for: beq $t0, $t1 fim #jump fim se $t0 = 8
-     div $t0, $t1
+for: beq  $t0, $t1, fim #jump fim se $t0 = 8
+     div  $t0, $t3
      mfhi $t2 #resto
-     beq $t2, $zero par
+     beq  $t2, $zero, par
      j impar
 
 par: mult $t0, $t3
      mflo $s1
-     sw $s1, 0($s0)
+     sw   $s1, 0($s0)
      addi $t0, $t0, 1 #i++
-     addi $t4, $t4, 1 #i(i-1)++
+     addi $s0, $s0, 4 #próxima posição do vetor
      j for 
      
-impar: add $s2, $t0, $t4
-       sw $s2, 4($s0)
+impar: lw  $t5, 0($s0)   #vetor[i]
+       lw  $t6, -4($s0)  #vetor[i-1]
+       add $t7, $t5, $t6 #[i] + [i-1]
+       sw  $t7, 0($s0)   #guarda $t7 na memoria
        
+       addi $t0, $t0, 1 #i++
+       addi $s0, $s0, 4 #próxima posição do vetor
+       j for
 fim:
--- lista-05/ex05.asm
+++ lista-05/ex05.asm
@@ -1,4 +1,36 @@
 #Escreva um programa que receba dois números em complemento de dois (armazenados em $s0 e $s1). 
-#Procure em um vetor (com início endereçado em $s3 etamanho armazenado em $s4) quantos números 
-#existem com valores dentro dointervalo fechado definido por $s0 e $s1. Assuma que $s0 ≤ $s1.
-#Utilize os registradores $s3 e $s4 apenas para armazenar o início e o tamanho dovetor.
+#Procure em um vetor (com início endereçado em $s3 e tamanho armazenado em $s4) quantos números 
+#existem com valores dentro do intervalo fechado definido por $s0 e $s1. Assuma que $s0 ≤ $s1.
+#Utilize os registradores $s3 e $s4 apenas para armazenar o início e o tamanho do vetor.
+
+.data
+vetor_x: .word -20, -15, -12, -8, -3, 1, 0, 5
+
+.text
+addi $s0, $zero, -14 #inicio intervalo
+addi $s1, $zero, -1 #fim intervalo
+addi $s4, $zero, 8 #tamanho vetor
+lui $s3, 0x1001 #reg base
+
+add $t0, $zero, $zero #i = 0
+add $t1, $zero, $zero #contador = 0
+add $t2, $zero, $s3#ponteiro
+
+loop: beq $t0, $s4, fim
+      lw $t3, 0($t2) #carrega valor
+      slt $t4, $t3, $s0 #numero < $s0
+      bne $t4, $zero, fora #se 1, fora do intervalo 
+      slt $t4, $s1, $t3 #se 0, dentro do intervalo
+      bne $t4, $zero, fora
+      j dentro
+      
+fora: addi $t0, $t0, 1 #i++
+      addi $t2, $t2, 4 #avança o vetor
+      j loop
+    
+dentro: addi $t1, $t1, 1 #contador++
+        addi $t0, $t0, 1 #i++
+        addi $t2, $t2, 4 #avança o vetor
+        j loop      
+          
+fim:
